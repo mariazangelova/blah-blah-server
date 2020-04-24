50988f1ff582c8e12c722bfd5198797339fbe68d
mariazangelova <mariazangelova@gmail.com>
2020-04-24 11:43:10 +0200

    Google-cloud setup

--- index.js
+++ index.js
@@ -1,8 +1,42 @@
+require("dotenv").config();
 const express = require("express");
 const app = express();
+const vision = require("@google-cloud/vision");
+var bodyParser = require("body-parser");
+// const db = require("db");
+// db.connect({
+//   host: process.env.DB_HOST,
+//   username: process.env.DB_USER,
+//   password: process.env.DB_PASS,
+// });
+
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }));
+
+// parse application/json
+app.use(bodyParser.json());
+
+const cors = require("cors");
+app.use(cors());
 
 app.get("/", (req, res) => {
   res.send("Hi from express");
+});
+
+app.post("/google-vision", cors(), (req, res) => {
+  async function quickstart() {
+    const imageUrl = req.body.imageUrl;
+    // Create a client
+    const client = new vision.ImageAnnotatorClient();
+
+    // Performs label detection on the image file
+    const [result] = await client.labelDetection(imageUrl);
+    const labels = result.labelAnnotations;
+    //console.log("Labels:");
+    //labels.forEach((label) => console.log(label.description));
+    res.status(200).send(labels);
+  }
+  quickstart();
 });
 
 const PORT = process.env.PORT || 4000;
